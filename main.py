944490ffe573bd6d281e50aa7018f93219b6d4b8
spiritbro1 <rinoakbr@gmail.com>
2020-07-27 13:40:23 +0700

    fix all just need to deploy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from all_link.start_all import start_all
 # from mysqls.pandasql import Links
-# from all_link.link12 import link12
-# link12()
+# from all_link.link13 import link13
+# link13()
 
 start_all()
 
