c35ffd01b2a6bd0bf159f2f9fa0fc6b1f3965516
spiritbro1 <rinoakbr@gmail.com>
2020-08-02 09:18:49 +0700

    link 33

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from all_link.start_all import start_all
 from mysqls.pandasql import Links,deleteWhere
-from all_link.link34 import link34
+# from all_link.link34 import link34
 # link34()
 # deleteWhere(LA_name="Bristol, City of")
 start_all()
