744698079346f1d2fd86ace5f2cb0c881a3ed974
resaqulyubi <resa@majoo.id>
2020-08-16 11:31:25 +0700

    link106

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from all_link.start_all import start_all
 from mysqls.pandasql import Links,deleteWhere,deleteAll
 deleteAll()
-from all_link.link103 import link103
-link103()
+from all_link.link106 import link106
+link106()
 
 # deleteWhere(LA_name="County Durham")
 # start_all()
