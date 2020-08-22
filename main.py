518d4651c722188b01dbb4a8e63d9c4abe439a07
spiritbro1 <62529025+spiritbro1@users.noreply.github.com>
2020-08-23 06:22:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from all_link.start_all import start_all
 from mysqls.pandasql import Links,deleteWhere,deleteAll
-import fileinput
-print("DO YOU WANT TO SCRAPE (y/N):")
-for line in fileinput.input():
-    if line.rstrip().lower() == "y":
-        deleteAll()
-        from all_link.link141 import link141
-        link141()
-    else:
-        break
+# import fileinput
+# print("DO YOU WANT TO SCRAPE (y/N):")
+# for line in fileinput.input():
+#     if line.rstrip().lower() == "y":
+#         deleteAll()
+#         from all_link.link141 import link141
+#         link141()
+#     else:
+#         break
 
 
 
 # deleteWhere(LA_name="County Durham")
-# start_all()
+start_all()
 
 
 
